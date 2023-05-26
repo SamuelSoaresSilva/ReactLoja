93ecdbeef3ec694411c071beb99a7aaeb622cf82
SamuelSoaresSilva <samuel.soares.cunha64@gmail.com>
2023-05-26 14:57:09 -0300

    Commit1 ProjetoCru

---
+++ nonick/src/App.js
@@ -0,0 +1,17 @@
+
+
+function App() {
+  return (
+    <div className="container">
+      <h1>NoNick</h1>
+      <div className="containerInput">
+        <input
+        type="text"
+        placeholder="Buscar"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default App;
