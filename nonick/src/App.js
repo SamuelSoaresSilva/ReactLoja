b93aa5c3f55fa991f75e49c8df59d4d68a52315b
Zuk3s <felipemarcedo_@hotmail.com>
2023-05-28 22:01:55 -0300

    Update

--- nonick/src/App.js
+++ nonick/src/App.js
@@ -1,12 +1,11 @@
-import Header from "./components/header";
+import Header from "./components/Header/Header";
 
-function App() {
+const array[];
+
+export default function App() {
   return (
     <div className="App">
       <Header />
-
     </div>
   );
 }
-
-export default App;
