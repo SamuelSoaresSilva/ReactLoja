63b45fd8c0179f3b991f5d48e0ac1f939ab17c41
Zuk3s <felipemarcedo_@hotmail.com>
2023-05-29 14:34:28 -0300

    Update

--- nonick/src/App.js
+++ nonick/src/App.js
@@ -1,6 +1,4 @@
-import Header from "./components/Header/Header";
-
-const array[];
+import Header from "./components/Header/Header.js";
 
 export default function App() {
   return (
--- nonick/src/components/Header/header.js
+++ nonick/src/components/Header/header.js
@@ -1,27 +1,40 @@
+export default function Headers() {
+    return (
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+            <div class="container-fluid">
+                <nav class="col nav">
+                    <a class="nav-link text-dark border-end" href="#">Login</a>
+                    <a class="nav-link text-dark" href="#">Registre-se</a>
+                </nav>
 
-export default function Header(){
-    return(
-    <header>
-        <a href="">fale conosco</a>
-        <img src="./Imagens/noNickLogo1.png"></img>
-        <div>
-            <a href=" ">
-                <img src="./Icons/cart_.png">
-                
-                </img>
-            </a>
-            <a href=" ">
-                <img src="./Icons/heart_.png">
+                <img class="text-center" src="./noNickLogo1.png" alt="Logo NoNick" width="250px"></img>
 
-                </img>
-            </a>
-            <a href=" ">
-                <img src="./Icons/search_.png">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+                    aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
 
-                </img>
-            </a>
-            
-        </div>
-    </header>
+                <div class="col justify-content-end collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">
+                                <img src="cart_.png" alt="Carrinho"></img>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">
+                                <img src="heart_" alt="Amei"></img>
+                            </a>
+                        </li>
+                    </ul>
+                    <form class="d-flex ms-3" role="search">
+                        <input class="form-control" type="search" placeholder="Search" aria-label="Search"></input>
+                        <button class="btn" type="submit">
+                            <img src="..\Repositórios GitHub\Sites-Web-Exemplos\ReactLoja\nonick\public\Icons\search_.png" alt="Pesquisa"></img>
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </nav>
     );
 }
